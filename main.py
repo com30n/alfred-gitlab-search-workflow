8bf6f460235b62027ba15168542bfc1e55900bfe
com30n <evgeniy.shubin.android@gmail.com>
2020-11-22 21:00:18 +0200

    Change database type to FTS3; improve accounts management

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
-
-import search
-import accounts
-import update_index
-from clone import get_clone_links
 
 from workflow import Workflow3
 
-from constants import CMD_SEARCH, CMD_UPDATE, CMD_ACCOUNT_LIST, CMD_ADD_ACCOUNT, CMD_DEL_ACCOUNT, CMD_CLONE
-
-manage_cmd = [
-    {'name': 'CMD_SEARCH', 'item': CMD_SEARCH, 'action': search.main},
-    {'name': 'CMD_UPDATE', 'item': CMD_UPDATE, 'action': update_index.main},
-    {'name': 'CMD_ACCOUNT_LIST', 'item': CMD_ACCOUNT_LIST, 'action': accounts.ManageGitlabAccounts.print_all_accounts},
-    {'name': 'CMD_CLONE', 'item': CMD_CLONE, 'action': get_clone_links},
-    {'name': 'CMD_ADD_ACCOUNT', 'item': CMD_ADD_ACCOUNT, },
-    {'name': 'CMD_DEL_ACCOUNT', 'item': CMD_DEL_ACCOUNT, },
-]
+from constants import MAIN_MANAGE_CMD, ACCOUNTS_MANAGE_CMD
 
 
 def get_cmd_from_var(wf):
@@ -26,45 +13,32 @@ def get_cmd_from_var(wf):
         cmd = wf.getvar('cmd')
     elif len(wf.args) > 0 and wf.args[0] != u'':
         cmd = wf.args[0]
+    else:
+        cmd = os.environ.get("cmd")
 
     logger.debug("CMD was set: %s" % cmd)
     return cmd
 
 
-def run_cmd(command):
-    for cmd in manage_cmd:
-        # If item has an action, just run it
-        if command == cmd['name'] and cmd.get('action'):
-            func = cmd['action']
-            return func()
-        # If item doesn't have an action, provide item['arg'] as a 'query' to a next filter
-        # and it will decide which of the chains of functions it has to execute
-        elif command == cmd['name']:
-            print(cmd['name'])
-            return
-
-
-def show_manager_menu(wf):
-    for cmd in manage_cmd:
-        it = wf.add_item(**cmd['item'])
-        it.setvar('cmd', cmd['name'])
+def show_manager_menu(wf, cmd_enum):
+    for cmd_name, cmd in cmd_enum.__members__.items():
+        it = wf.add_item(**cmd.value)
+        it.setvar('cmd', cmd_name)
     return wf
 
 
-def manager(wf):
-    # Get a 'cmd' variable, if it exists into the context
+def main(wf):
     cmd = get_cmd_from_var(wf)
+    if cmd and cmd == "CMD_ACCOUNTS":
+        wf = show_manager_menu(wf, cmd_enum=ACCOUNTS_MANAGE_CMD)
+    else:
+        wf = show_manager_menu(wf, cmd_enum=MAIN_MANAGE_CMD)
 
-    # If manager run not as a manager ('glm'), it has to just only run a chosen command
-    if cmd:
-        return run_cmd(cmd)
-
-    # If manager run as a manager ('glm'), it has to show the main menu
-    wf = show_manager_menu(wf)
     wf.send_feedback()
 
 
 if __name__ == u'__main__':
     wf = Workflow3()
     logger = wf.logger
-    sys.exit(wf.run(manager))
+    sys.exit(wf.run(main))
+
--- utils.py
+++ utils.py
@@ -4,11 +4,18 @@ from workflow import Workflow3
 wf = Workflow3()
 
 
-def get_wf_variable(var_name):
+def get_wf_variable(wf, var_name, default=""):
     """Returns workflow variable, if exists. If doesn't - return value from OS environ."""
     if wf.variables and wf.getvar(var_name):
-        return wf.getvar(var_name)
+        result = wf.getvar(var_name)
     else:
-        return os.getenv(var_name)
+        result = os.getenv(var_name)
 
+    if not result:
+        return default
+    return result
 
+def merge_two_dicts(x, y):
+    z = x.copy()  # start with x's keys and values
+    z.update(y)  # modifies z with y's keys and values & returns None
+    return z
